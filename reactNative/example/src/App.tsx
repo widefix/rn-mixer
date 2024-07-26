14e85d199c460b9e0c7e17a89f7a929a389db7cc
Great Armstrong <greatarmstrong@ArmstrongsMBP2.lan>
2024-07-26 01:44:58 +0100

    completed bridge for android

--- reactNative/example/src/App.tsx
+++ reactNative/example/src/App.tsx
@@ -11,17 +11,17 @@ export default function App() {
   const [playBackProgress, setplayBackProgress] = React.useState(0);
   const [errMessage, seterrMessage] = React.useState(":::error messages:::");
   const audioUrls: string[] = [
-    // "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/226/original/Way_Maker__0_-_E_-_Original_--_1-Click.m4a",
-    // "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/228/original/Way_Maker__0_-_E_-_Original_--_2-Guide.m4a",
+    "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/226/original/Way_Maker__0_-_E_-_Original_--_1-Click.m4a",
+    "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/228/original/Way_Maker__0_-_E_-_Original_--_2-Guide.m4a",
     "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/236/original/Way_Maker__0_-_E_-_Original_--_11-Lead_Vox.m4a",
     "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/227/original/Way_Maker__0_-_E_-_Original_--_3-Drums.m4a",
     "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/229/original/Way_Maker__0_-_E_-_Original_--_4-Percussion.m4a",
     "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/232/original/Way_Maker__0_-_E_-_Original_--_5-Bass.m4a",
     "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/230/original/Way_Maker__0_-_E_-_Original_--_6-Acoustic.m4a",
-    // "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/234/original/Way_Maker__0_-_E_-_Original_--_7-Electric_1.m4a",
-    // "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/235/original/Way_Maker__0_-_E_-_Original_--_8-Electric_2.m4a",
-    // "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/237/original/Way_Maker__0_-_E_-_Original_--_9-Main_Keys.m4a",
-    // "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/231/original/Way_Maker__0_-_E_-_Original_--_10-Aux_Keys.m4a",
+    "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/234/original/Way_Maker__0_-_E_-_Original_--_7-Electric_1.m4a",
+    "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/235/original/Way_Maker__0_-_E_-_Original_--_8-Electric_2.m4a",
+    "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/237/original/Way_Maker__0_-_E_-_Original_--_9-Main_Keys.m4a",
+    "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/231/original/Way_Maker__0_-_E_-_Original_--_10-Aux_Keys.m4a",
     "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/238/original/Way_Maker__0_-_E_-_Original_--_12-Soprano_Vox.m4a",
     "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/239/original/Way_Maker__0_-_E_-_Original_--_13-Tenor_Vox.m4a",
     "https://cdn.worshiponline.com/estp-public/song_audio_mixer_tracks/audios/000/034/233/original/Way_Maker__0_-_E_-_Original_--_14-Choir.m4a"
@@ -29,27 +29,34 @@ export default function App() {
 
   React.useEffect(() => {
     const subscription = armsaudioEmitter.addListener('DownloadProgress', (event) => {
+      console.log(event);
       setProgress(event.progress);
     });
 
     const subscriptionTracks = armsaudioEmitter.addListener('DownloadComplete', (event) => {
-      setTracks(event.fileNames);
+      console.log(event);
+      setTracks(event);
     });
 
     const subscriptionPlaybackUpdate = armsaudioEmitter.addListener('PlaybackProgress', (event) => {
+      console.log(event);
+      // setplayBackProgress(event);
       setplayBackProgress(event.progress);
     });
 
     const subscriptionDownloadErrorMessage = armsaudioEmitter.addListener('DownloadErrors', (event) => {
-      seterrMessage(event.errMsg);
+      console.log(event);
+      seterrMessage(event);
     });
 
     const subscriptionTracksAmplitude = armsaudioEmitter.addListener('TracksAmplitudes', (event) => {
-      console.log(event.amplitudes);
+     // console.log(event);
+      // console.log(event.amplitudes);
+      // console.log(event.amplitudes);
     });
 
     const subscriptionDownloadStart = armsaudioEmitter.addListener('DownloadStart', (event) => {
-      console.log(event.status);
+      console.log(event); // for download just use event
     });
 
 
@@ -83,12 +90,14 @@ export default function App() {
       <Text style={{ color: "grey" }}>Download Progress: {progress * 100}%</Text>
       <Button title='Open file' onPress={() => xmod.newAddon().pickAudioFile()}></Button>
 
+
+      <View style={{ height: 20, marginVertical: 3 }}></View>
       <Button title='Used Download files' onPress={() => xmod.newAddon().downloadAudioFiles(audioUrls)}></Button>
-
+      <View style={{ height: 20, marginVertical: 3 }}></View>
       <Button title='Play Mix' onPress={() => xmod.newAddon().playAudio()}></Button>
-
+      <View style={{ height: 20, marginVertical: 3 }}></View>
       <Button title='Pause/Resume' onPress={() => xmod.newAddon().pauseResumeMix()}></Button>
-
+      <View style={{ height: 20, marginVertical: 3 }}></View>
       <Text style={{ color: "grey" }}>Playback & Tracks: </Text>
       <PlayBackSlider
         initialValue={playBackProgress}
